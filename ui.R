f8cbc1b111f525c17c3493c2d22bfa4d37a6d1b1
Jenny <Nhuy@Jennifers-MacBook-Air.local>
2017-08-27 12:38:10 -0400

    added css stylesheet

--- ui.R
+++ ui.R
@@ -18,29 +18,35 @@ dashboardPage(
   dashboardSidebar(disable = TRUE),
 
   # app main body
-  dashboardBody(verticalLayout(
-    tabBox(width = NULL, selected = "Meals",
+  dashboardBody(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.css")
+    ),
 
-      # meal options
-      tabPanel("Meals",
-               # inputs: choose ingredients to use
-               uiOutput("input_selection_options"),
+    verticalLayout(
+      tabBox(width = NULL, selected = "Meals",
 
-               # outputs: links to matched recipes
-               box(title = "Recipes", width = NULL, solidHeader = TRUE, status = "primary",
-                   uiOutput("randomize"), h1(), h1(),
-                   uiOutput("recipe_options")
-               )
-      ),
+        # meal options
+        tabPanel("Meals",
+                 # inputs: choose ingredients to use
+                 uiOutput("input_selection_options"),
 
-      # dessert options
-      tabPanel("Desserts",
-               uiOutput("randomize_dessert"),
-               h1(), h1(),
-               uiOutput("desserts")
+                 # outputs: links to matched recipes
+                 box(title = "Recipes", width = NULL, solidHeader = TRUE, status = "primary",
+                     uiOutput("randomize"), h1(), h1(),
+                     uiOutput("recipe_options")
+                 )
+        ),
+
+        # dessert options
+        tabPanel("Desserts",
+                 uiOutput("randomize_dessert"),
+                 h1(), h1(),
+                 uiOutput("desserts")
+        )
+
       )
-
     )
-  ))
+  )
 )
 
